ae7a8d31696d25a95717c5baf3e6b8fcec66a387
garyellow <gary20011110@gmail.com>
2025-11-23 20:07:11 +0800

    chore: 移除更新腳本中的註解與多餘行，簡化執行流程

--- deployments/update.cmd
+++ deployments/update.cmd
@@ -1,5 +1,2 @@
-@echo off
-REM Update all services to latest images
 docker compose up -d --pull always
-REM Uncomment to clean up unused images:
-REM docker image prune -f
+# docker image prune -f
--- deployments/update.sh
+++ deployments/update.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-# Update all services to latest images
-cd "$(dirname "$0")"
 docker compose up -d --pull always
-# Uncomment to clean up unused images:
 # docker image prune -f
